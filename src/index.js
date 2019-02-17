4d0c8adc5ee91aff8526a587cdb739744cd3c461
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2019-02-17 11:38:24 -0300

    Configurando store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
 
 import '~/config/ReactotronConfig';
+import store from '~/store';
 
-const App = () => <View />;
+const App = () => (
+  <Provider store={store}>
+    <View />
+  </Provider>
+);
 
 export default App;
