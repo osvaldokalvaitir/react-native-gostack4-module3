a02507b867b624298b44edb80f4761dc02f8ba0b
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2019-02-21 20:00:54 -0300

    Favorito duplicado

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '~/services/api';
 
 import { addFavoriteSuccess, addFavoriteError } from '~/store/actions/favorites';
@@ -7,7 +7,13 @@ export function* addFavoriteRequest(action) {
   try {
     const response = yield call(api.get, `/repos/${action.payload.repoName}`);
 
-    yield put(addFavoriteSuccess(response.data));
+    const favorites = yield select(state => state.favorites.data);
+
+    if (favorites.find(favorite => favorite.id === response.data.id)) {
+      yield put(addFavoriteError('Repositório duplicado'));  
+    } else {
+      yield put(addFavoriteSuccess(response.data));
+    }    
   } catch (err) {
     yield put(addFavoriteError('Repositório não existe'));
   }
